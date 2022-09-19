f63fb0cdabdfc441e6b286c9fdb8ac7107d47267
KIMJUNKYUNG <52786004+KIMJUNKYUNG@users.noreply.github.com>
2022-09-19 10:20:40 +0800

    clientScoket instance can't be send with emit

--- TouchFasterServer/server.js
+++ TouchFasterServer/server.js
@@ -6,27 +6,22 @@ const http = require('http')
 const server = http.createServer(app)
 const io = new Server(server)
 
-const Home = io.of('/')
-const rooms = [
-    {
-        "roomName": "Hello",
-        "owner": "Tester",
-        "opponent": "Junkyung"
-    }
-]
+const rooms = []
 
 const { getRandomFloat } = require('./utils')
 
 let broadcastRoomList = () => {
     console.log("send Room List, ", rooms)
-    Home.emit('roomList', { rooms })
+    io.emit('roomList', { rooms })
 }
 
 function makeRoom(ownerSocket, roomName) {
     console.log(`make Room, OwnerId : ${ownerSocket.id}, roomName ${roomName}`)
     // ownerSocket.join(roomName)
+    console.log(ownerSocket.id)
     rooms.push({
-        roomName, ownerSocket,
+        roomName,
+        "owner": ownerSocket.id,
         "opponent": undefined
     })
 }
@@ -41,20 +36,16 @@ function deleteRoom(ownerId) {
     // }
 }
 
-Home.on('connection', (clientSocket) => {
+io.on('connection', (clientSocket) => {
     console.log(`room connection, socketId : ${clientSocket.id}`)
     broadcastRoomList()
 
     clientSocket.on('makeRoom', (roomName) => {
         console.log("makeRoom, rooName : ", roomName)
-        // clientSocket.join('Hello')
+        clientSocket.join('Hello')
         makeRoom(clientSocket, roomName)
         // broadcastRoomList()
     })
-    clientSocket.on('test', () => {
-        Home.to('Hello').emit("test", "World")
-    })
-
     clientSocket.on('joinRoom', (roomNumber) => {
 
     })
