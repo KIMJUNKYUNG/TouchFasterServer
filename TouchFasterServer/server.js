99ac9ff851d5cd11dbf30ee4b2043cd6ba711b99
KIMJUNKYUNG <52786004+KIMJUNKYUNG@users.noreply.github.com>
2022-09-20 13:30:38 +0800

    Add Ready function, Room number

--- TouchFasterServer/server.js
+++ TouchFasterServer/server.js
@@ -7,55 +7,72 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 const rooms = []
+const currentUsers = []
 
 const { getRandomFloat } = require('./utils')
 
 let broadcastRoomList = () => {
-    console.log("send Room List, ", rooms)
+    console.log("send Room Count, ", rooms.length)
+
     io.emit('roomList', { rooms })
 }
 
-function makeRoom(ownerSocket, roomName) {
-    console.log(`make Room, OwnerId : ${ownerSocket.id}, roomName ${roomName}`)
-    // ownerSocket.join(roomName)
-    console.log(ownerSocket.id)
+function makeRoom(ownerSocket, roomNumber, roomName) {
+    console.log(`make Room, OwnerId : ${ownerSocket.id}, roomNumber : ${roomNumber}, roomName ${roomName}`)
+
+    ownerSocket.join(roomName)
     rooms.push({
+        "roomNumber": roomNumber,
         roomName,
         "owner": ownerSocket.id,
-        "opponent": undefined
+        "ownerReady": false,
+        "clientReady": false
     })
 }
+function joinRoom(clientSocket, roomName) {
+    console.log(`join Room,  : cleintId : ${clientSocket.id}, roomName ${roomName}`)
 
+    clientSocket.join(roomName)
+
+    io.to(roomName).emit("playerJoined");
+}
 
 function deleteRoom(ownerId) {
     console.log(`delete Room, OwnerId : ${ownerId} `)
+
     let index = rooms.findIndex(element => element.owner === ownerId)
     if (index !== -1) {
         rooms.splice(index, 1)
+        currentUsers.splice(index, 1)
         broadcastRoomList()
     }
 }
 
 io.on('connection', (clientSocket) => {
     console.log(`room connection, socketId : ${clientSocket.id}`)
+
+    currentUsers.push(clientSocket)
     broadcastRoomList()
 
     clientSocket.on('makeRoom', (roomName) => {
-        console.log("makeRoom, rooName : ", roomName)
-        clientSocket.join('Hello')
-        makeRoom(clientSocket, roomName)
-        // broadcastRoomList()
+        let roomNumber = (rooms.length + 1).toString()
+        makeRoom(clientSocket, roomNumber, roomName)
+        broadcastRoomList()
     })
     clientSocket.on('joinRoom', (roomNumber) => {
+        let roomName = rooms[roomNumber].roomName
+        joinRoom(clientSocket, roomName)
+    })
+    clientSocket.on('ready', (bReady, a, b) => {
+        console.log(`Ready, socketId : ${clientSocket.id}, condition : ${bReady}, ${a}, ${b}`)
 
     })
+
     clientSocket.on('disconnect', () => {
         console.log(`Disconnect, socektId : ${clientSocket.id}`)
         deleteRoom(clientSocket.id)
     })
-    clientSocket.on('ready', () => {
-        console.log(`Ready, socketId : ${clientSocket.id}`)
-    })
+
 })
 
 server.listen(3000, async () => {
